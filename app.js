e9d8335de076deabeb71ab936310c36a4ad1a8b5
Damiano Braga <damiano.braga@gmail.com>
2013-01-09 00:27:17 -0800

    moved from classes to object literals

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var app = express();
 
-
 var exec = require('child_process').exec;
+var gitLog = {
+	log: "",
+	ncommits : 0, 
+	commits : [],
+  parse: function(log) {
+      this.log = log.split("\n");
+      this.ncommits = (this.log.length / 6);
+      this.commits = [];
+      for (var i=0; i< this.log.length; i+=(this.ncommits-1)){
+      		var commit ={
+      				// Commit hash line -> commit: {hash_commit}
+      				hash : this.log[i+0].split(" ")[1],
+       			// Author line -> Author: {name} <{email}>
+        		authorInfo : {
+        				name: this.log[i+1].split(" ")[1],
+        				email: this.log[i+1].split(" ")[2]
+        		}, 
+        		// Date line -> Date: {date}
+        		date : this.log[i+2].split("Date:   ")[1],
+        		// Comment line -> {comment}
+        		comment : this.log[i+4].split("    ")[1]       		         		     				
+      		}
+      		this.commits.push( commit )
+      }
+      return this.commits;
+  },
+  toString: function() {
+      return log;
+  }
+}; 
 
 exec('git log', function (error, gitLogOutput) {
-    var log = new gitLog(gitLogOutput);
+    var log = gitLog.parse(gitLogOutput);
     // Debug mode. printing out the first commit comment
-    console.log(log.commits[0].comment);
+    console.log(log[0]["comment"]);
 });
 
 
