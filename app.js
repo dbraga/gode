6127493b5bb078d1d018d68b5ebc7609bcc58118
Damiano Braga <damiano.braga@gmail.com>
2013-01-08 23:33:56 -0800

    call to exec the command git log and parse the output

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+
+var exec = require('child_process').exec;
+
+exec('git log', function (error, gitLogOutput) {
+    var log = new gitLog(gitLogOutput);
+    // Debug mode. printing out the first commit comment
+    console.log(log.commits[0].comment);
+});
+
+
+
 app.get('/', function(req, res){
   res.send('Hello World');
 });
