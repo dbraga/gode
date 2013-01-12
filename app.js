eac133d8df11c97a3c2c251a6cd6201653e422de
Damiano Braga <damiano.braga@gmail.com>
2013-01-11 19:11:31 -0800

    converted tabs to spaces

--- app.js
+++ app.js
@@ -3,37 +3,37 @@ var app = express();
 
 var exec = require('child_process').exec;
 var gitLog = {
-		log: "",
-		ncommits : 0, 
-		commits : [],
-		parse: function(log) {
-			this.log = log.split("\n");
-			this.ncommits = (this.log.length / 6);
-			this.commits = [];
-			for (var i=0; i< this.log.length; i+=(this.ncommits-1)){
-					var commit ={
-							// Commit hash line -> commit: {hash_commit}
-							hash : this.log[i+0].split(" ")[1],
-							// Author line -> Author: {name} <{email}>
-							authorInfo : {	name: this.log[i+1].split(" ")[1], email: this.log[i+1].split(" ")[2] }, 
-							// Date line -> Date: {date}
-							date : this.log[i+2].split("Date:   ")[1],
-							// Comment line -> {comment}
-							comment : this.log[i+4].split("    ")[1]       		         		     				
-					}
-					this.commits.push( commit )
-			}
-			return this.commits;
-		},
-		toString: function() {
-			return log;
-		}
+    log: "",
+    ncommits : 0, 
+    commits : [],
+    parse: function(log) {
+      this.log = log.split("\n");
+      this.ncommits = (this.log.length / 6);
+      this.commits = [];
+      for (var i=0; i< this.log.length; i+=(this.ncommits-1)){
+          var commit ={
+              // Commit hash line -> commit: {hash_commit}
+              hash : this.log[i+0].split(" ")[1],
+              // Author line -> Author: {name} <{email}>
+              authorInfo : {  name: this.log[i+1].split(" ")[1], email: this.log[i+1].split(" ")[2] }, 
+              // Date line -> Date: {date}
+              date : this.log[i+2].split("Date:   ")[1],
+              // Comment line -> {comment}
+              comment : this.log[i+4].split("    ")[1]                                  
+          }
+          this.commits.push( commit )
+      }
+      return this.commits;
+    },
+    toString: function() {
+      return log;
+    }
 }; 
 
 exec('git log', function (error, gitLogOutput) {
-		var log = gitLog.parse(gitLogOutput);
-		// Debug mode. printing out the first commit comment
-		console.log(log[0]["comment"]);
+    var log = gitLog.parse(gitLogOutput);
+    // Debug mode. printing out the first commit comment
+    console.log(log[0]["comment"]);
 });
 
 
